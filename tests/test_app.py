daa858a64141c8879363fd3c92aecaf9a2283983
Saksham Kamra <sakshamkamra33@gmail.com>
2026-04-07 00:25:08 +0530

    Fix tests after UI update

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,17 +4,21 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app
+
 def test_home():
+    app.testing = True
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
 
 def test_about():
+    app.testing = True
     client = app.test_client()
     response = client.get('/about')
     assert response.status_code == 200
 
 def test_status():
+    app.testing = True
     client = app.test_client()
     response = client.get('/status')
     assert response.status_code == 200
