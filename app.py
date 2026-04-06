c9b6f52e75c02589ba23ba649f84d655329483cf
Saksham Kamra <sakshamkamra33@gmail.com>
2026-04-07 00:18:46 +0530

    Added UI to project

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Welcome to DevOps Project 🚀")
+    return render_template("index.html")
+
+@app.route('/api/status')
+def status():
+    return jsonify(status="Running", version="1.0.0")
 
 @app.route('/about')
 def about():
-    return jsonify(message="This project demonstrates CI/CD pipeline")
-
-@app.route('/status')
-def status():
-    return jsonify(status="Running", version="1.0")
+    return jsonify(message="This project demonstrates CI/CD pipeline with Docker.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
